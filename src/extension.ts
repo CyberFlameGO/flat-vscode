7f308efeb2d4813a80cc4c6e1c554e0af0be06eb
Matt Rothenberg <mattrothenberg@github.com>
2021-02-12 16:47:27 -0500

    feat: get URL and CRON schedule from user

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import cronstrue from "cronstrue";
 
 import { Credentials } from "./credentials";
 import { RepositoriesProvider } from "./providers";
@@ -11,28 +12,48 @@ export async function activate(context: vscode.ExtensionContext) {
   const octokit = await credentials.getOctokit();
 
   context.subscriptions.push(
-    vscode.commands.registerCommand(
-      "data-experiment.getGitHubUser",
-      async () => {
-        const octokit = await credentials.getOctokit();
-        const userInfo = await octokit.users.getAuthenticated();
+    vscode.commands.registerCommand("flat.getGitHubUser", async () => {
+      const octokit = await credentials.getOctokit();
+      const userInfo = await octokit.users.getAuthenticated();
 
-        vscode.window.showInformationMessage(
-          `Logged into GitHub as ${userInfo.data.login}`
-        );
-      }
-    )
+      vscode.window.showInformationMessage(
+        `Logged into GitHub as ${userInfo.data.login}`
+      );
+    })
   );
 
   context.subscriptions.push(
     vscode.commands.registerCommand(
-      "data-experiment.openIssueOnGitHub",
+      "flat.openIssueOnGitHub",
       (issueUrl: string) =>
         vscode.commands.executeCommand(
           "vscode.open",
           vscode.Uri.parse(issueUrl)
         )
     )
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("flat.createAction", async () => {
+      const dataSourceUrl = await vscode.window.showInputBox({
+        prompt: "Enter full URL to data source",
+      });
+
+      const cronSchedule = await vscode.window.showInputBox({
+        prompt: "Enter CRON schedule",
+      });
+
+      if (cronSchedule) {
+        const parsed = cronstrue.toString(cronSchedule);
+
+        vscode.window.showInformationMessage(
+          `Success! Your action will run ${parsed}`
+        );
+      }
+
+      // TODO:
+      // Fill GitHub action template with source URL and cron schedule value
+    })
   );
 
   const reposProvider = new RepositoriesProvider(octokit);
