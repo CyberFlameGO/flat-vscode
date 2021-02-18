a9f0519f665e41f171607df16a95fb6e396aab3e
Matt Rothenberg <mattrothenberg@github.com>
2021-02-18 15:34:51 -0500

    wip, form submissions

--- src/commands/create-action.ts
+++ src/commands/create-action.ts
@@ -3,7 +3,7 @@ import cronstrue from "cronstrue";
 
 import { buildVirtualDocument, isValidUrl } from "../lib";
 
-export const createAction = async () => {
+function createHTMLAction() {
   const sourceInputBox = vscode.window.createInputBox();
   sourceInputBox.placeholder = "URL";
   sourceInputBox.title = "Enter fully qualified URL of data source";
@@ -66,6 +66,117 @@ export const createAction = async () => {
     } else {
       nameInputBox.validationMessage =
         "You need to enter a name for the action.";
+    }
+  });
+}
+
+function getSqlWebViewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SQL: Database Connection Info</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <script type="text/javascript">
+      function app () {
+        return {
+          form: {
+            host: "",
+            port: "",
+            username: "",
+            password: "",
+            database: ""
+          },
+          isTesting: false,
+          handleTest () {
+            this.isTesting = true;
+          },
+          handleSubmit (e) {
+            e.preventDefault();
+            console.log(e)
+          }
+        }
+      }
+    </script>  
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+</head>
+<body class="p-4">
+  <div x-data="app()">
+    <form class="space-y-4 flex flex-col" @submit="handleSubmit">
+      <input x-model="form.host" class="p-2 text-sm block text-black" placeholder="Host" />
+      <input x-model="form.port" class="p-2 text-sm block text-black" placeholder="Port " />
+      <input x-model="form.username" class="p-2 text-sm block text-black" placeholder="Username " />
+      <input x-model="form.password" class="p-2 text-sm block text-black" placeholder="Password " />
+      <input x-model="form.database" class="p-2 text-sm block text-black" placeholder="Database " />
+      <div class="flex space-x-4">
+        <button type="submit" class="bg-blue-700 text-white p-2 text-sm flex-1">Submit</button>
+        <button type="button" @click="handleTest" class="bg-blue-700 text-white p-2 text-sm flex-shrink-0">
+          <template x-if="isTesting">
+            <span>Testing...</span>
+          </template>
+          <template x-if="!isTesting">
+            <span>Test Connection</span>
+          </template>
+        </button>
+      </div>
+    </form>
+  </div>
+  
+</body>
+
+</html>`;
+}
+
+function createSQLAction() {
+  const panel = vscode.window.createWebviewPanel(
+    "flatSql",
+    "SQL: Database Connection Info",
+    vscode.ViewColumn.One,
+    {
+      enableScripts: true,
+    }
+  );
+
+  panel.webview.html = getSqlWebViewContent();
+}
+
+interface ActionItem extends vscode.QuickPickItem {
+  value: string;
+}
+
+export const createAction = async () => {
+  const quickpick = vscode.window.createQuickPick<ActionItem>();
+  quickpick.ignoreFocusOut = true;
+  quickpick.items = [
+    {
+      label: "SQL",
+      value: "sql",
+      description: "You'll be prompted for a connection string and a SQL query",
+    },
+    {
+      label: "HTML",
+      value: "html",
+      description: "You'll be prompted for a URL and a CRON schedule",
+    },
+  ];
+  quickpick.show();
+
+  quickpick.onDidAccept((e) => {
+    const [selected] = quickpick.selectedItems;
+    quickpick.hide();
+
+    if (selected) {
+      switch (selected.value) {
+        case "html":
+          createHTMLAction();
+          break;
+        case "sql":
+          createSQLAction();
+          break;
+        default:
+          break;
+      }
     }
   });
 };
