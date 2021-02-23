eb58f795072755c41d7a1694ca8a40e5f0638f52
Matt Rothenberg <mattrothenberg@github.com>
2021-02-23 10:52:24 -0500

    feat: remove SQL from global store

--- src/store.ts
+++ src/store.ts
@@ -2,27 +2,19 @@ import create from "zustand/vanilla";
 
 type FlatStore = {
   connectionString: string;
-  sql: string;
-  setSql: (sql: string) => void;
   setConnectionString: (sql: string) => void;
   reset: () => void;
 };
 
 const store = create<FlatStore>((set) => ({
-  sql: "",
   connectionString: "",
-  setSql: (sql: string) => {
-    set({
-      sql,
-    });
-  },
   setConnectionString: (connectionString: string) => {
     set({
       connectionString,
     });
   },
   reset: () => {
-    set({ connectionString: "", sql: "" });
+    set({ connectionString: "" });
   },
 }));
 
