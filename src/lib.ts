ba77bc761f491aa4912c4565ad03878113036c40
Matt Rothenberg <mattrothenberg@github.com>
2021-02-26 11:25:05 -0500

    feat: remove commit step from yaml template

--- src/lib.ts
+++ src/lib.ts
@@ -47,14 +47,6 @@ jobs:
         queryfile: query.sql`
             : "\n"
         }
-    - name: Commit and push if changed
-      run: |-
-        git config user.name "Flat"
-        git config user.email "actions@users.noreply.github.com"
-        git add -A
-        timestamp=$(date -u)
-        git commit -m "Latest data: \${timestamp}" || exit 0
-        git push
 `.replace(/^\s*[\r\n]/gm, "");
 }
 
